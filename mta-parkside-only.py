c56ee1e831e90971d6ed04ac9909715bc88e98db
Dan <dansushi@gmail.com>
2018-02-26 01:40:50 -0500

    Update mta-parkside-only.py

--- mta-parkside-only.py
+++ mta-parkside-only.py
@@ -2,6 +2,7 @@
 import sys
 import json
 import time
+import datetime
 from sys import exit
 
 from sense_hat import SenseHat
@@ -29,7 +30,29 @@ X = WHITE
 O = BLACK
 
 
-global variable
-
-print()
-
+import urllib.request, json 
+with urllib.request.urlopen("http://127.0.0.1:5000/by-id/78f3") as url: #check Parkside Only
+#with urllib.request.urlopen("http://127.0.0.1:5000/by-id/84ac") as url: #check Times Square Only
+		MTAPI_JSON = json.loads(url.read().decode()) # MTAPI_JSON is a dictionary
+		
+		northbound = MTAPI_JSON["data"][0]["N"] #show all northbound trains for Parkside
+		print("Northbound trains are:")
+		for x in northbound:
+			route = x["route"]
+			time = x["time"]
+			#sense.show_message(time) 
+			wait_in_minutes = 2 ## FIGURE OUT HOW TO CONVERT THIS DATETIME TO datetime format AND CALCULATE THE DIFFERENCE BETWEEN NOW AND ARRIVAL TIME
+			sense.show_message(str(wait_in_minutes))
+			print("Route is:", route)
+			print("Arrival time is:", time,"\n")
+				
+		print("")
+		
+		southbound = MTAPI_JSON["data"][0]["S"] #show all southbound trains for Parkside
+		print("Southbound trains are:")
+		for x in southbound:
+			route = x["route"]
+			time = x["time"]
+			print("Route is:", route)
+			print("Arrival time is:", time,"\n")
+			
