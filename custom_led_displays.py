100af3f8fb849e385a98e9947d30d3947f407e2d
Dan <dansushi@gmail.com>
2018-02-28 00:55:45 -0500

    Create custom_led_displays.py

---
+++ custom_led_displays.py
@@ -0,0 +1,203 @@
+def ones(T,B,digit):
+	if digit == "0":
+		led_display = [
+		B, T, T, T,
+		B, T, B, T,
+		B, T, B, T,
+		B, T, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "1":
+		led_display = [
+		B, B, T, B,
+		B, B, T, B,
+		B, B, T, B,
+		B, B, T, B,
+		B, B, T, B,
+		]
+		return led_display
+	elif digit == "2":
+		led_display = [
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		B, T, B, B,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit =="3":
+		led_display = [
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "4":
+		led_display = [
+		B, T, B, T,
+		B, T, B, T,
+		B, T, T, T,
+		B, B, B, T,
+		B, B, B, T,
+		]
+		return led_display
+	elif digit == "5":
+		led_display = [
+		B, T, T, T,
+		B, T, B, B,
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "6":
+		led_display = [
+		B, T, T, T,
+		B, T, B, B,
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "7":
+		led_display = [
+		B, T, T, T,
+		B, B, B, T,
+		B, B, B, T,
+		B, B, B, T,
+		B, B, B, T,
+		]
+		return led_display
+	elif digit == "8":
+		led_display = [
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "9":
+		led_display = [
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	else:
+		led_display = [
+		B, B, B, B,
+		B, B, B, B,
+		B, B, B, B,
+		B, B, B, B,
+		B, B, B, B,
+		]
+		return led_display
+
+def tens(T,B,digit):
+	if digit == "0":
+		led_display = [
+		B, T, T, T,
+		B, T, B, T,
+		B, T, B, T,
+		B, T, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "1":
+		led_display = [
+		B, B, T, B,
+		B, B, T, B,
+		B, B, T, B,
+		B, B, T, B,
+		B, B, T, B,
+		]
+		return led_display
+	elif digit == "2":
+		led_display = [
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		B, T, B, B,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit =="3":
+		led_display = [
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "4":
+		led_display = [
+		B, T, B, T,
+		B, T, B, T,
+		B, T, T, T,
+		B, B, B, T,
+		B, B, B, T,
+		]
+		return led_display
+	elif digit == "5":
+		led_display = [
+		B, T, T, T,
+		B, T, B, B,
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "6":
+		led_display = [
+		B, T, T, T,
+		B, T, B, B,
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "7":
+		led_display = [
+		B, T, T, T,
+		B, B, B, T,
+		B, B, B, T,
+		B, B, B, T,
+		B, B, B, T,
+		]
+		return led_display
+	elif digit == "8":
+		led_display = [
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	elif digit == "9":
+		led_display = [
+		B, T, T, T,
+		B, T, B, T,
+		B, T, T, T,
+		B, B, B, T,
+		B, T, T, T,
+		]
+		return led_display
+	else:
+		led_display = [
+		B, B, B, B,
+		B, B, B, B,
+		B, B, B, B,
+		B, B, B, B,
+		B, B, B, B,
+		]
+		return led_display
+
+		
