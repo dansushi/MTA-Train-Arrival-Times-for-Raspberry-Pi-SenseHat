9f14c76f96e5a405182119b78cc19f6b00df7ad7
Dan <dansushi@gmail.com>
2018-03-01 02:58:32 -0500

    Update custom_led_displays.py

--- custom_led_displays.py
+++ custom_led_displays.py
@@ -1,3 +1,132 @@
+def N_B_S_display(N_B_S):
+	
+	#Colors     r    g    b
+	b =		(  0,   0,   0) #BLACK BACKGROUND
+	f =		(155, 155, 155) #FOREGROUND
+		
+	if N_B_S == "N":
+		display = [
+			b, b, b, b, f, b, b, b,
+			b, b, b, f, f, f, b, b,
+			b, b, f, f, f, f, f, b,
+			b, f, f, f, f, f, f, f,
+			b, b, b, f, f, f, b, b,
+			b, b, b, f, f, f, b, b,
+			b, b, b, f, f, f, b, b,
+			b, b, b, f, f, f, b, b,
+		]
+		return display
+	elif N_B_S == "B":
+		display = [
+			b, b, b, b, b, b, b, b,
+			b, b, b, b, b, b, b, b,
+			b, b, b, f, f, b, b, b,
+			b, b, f, f, f, f, b, b,
+			b, b, f, f, f, f, b, b,
+			b, b, b, f, f, b, b, b,
+			b, b, b, b, b, b, b, b,
+			b, b, b, b, b, b, b, b,
+		]
+		return display
+	elif N_B_S == "S":
+		display = [
+			b, b, b, f, f, f, b, b,
+			b, b, b, f, f, f, b, b,
+			b, b, b, f, f, f, b, b,
+			b, b, b, f, f, f, b, b,
+			b, f, f, f, f, f, f, f,
+			b, b, f, f, f, f, f, b,
+			b, b, b, f, f, f, b, b,
+			b, b, b, b, f, b, b, b,
+		]
+		return display
+
+def station_map(current_station):
+	
+	#Colors     r    g    b
+	b =		(  0,   0,   0) #BLACK BACKGROUND
+	a =		(  10, 10,  10) #GRAY
+	w =		(255, 255, 255) #WHITE
+	o =		(255,  10,   0) #ORANGE LINE
+	y =		(255, 140,   0) #YELLOW LINE
+	g =		(  0, 200,   0) #GREEN LINE
+	r =		(255,   0,   0) #RED LINE
+	v =		(255,   0, 200) #PURPLE
+	s =		( 45, 172, 213) #STATION COLOR
+		
+	if current_station == "Parkside Ave":	#Make sure the station name is spelled right, exactly like in main file under station_list
+		map = [
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, s, s, b, b, g, r,
+			b, o, s, s, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+		]
+		return map
+	elif current_station == "Church Ave":	#Make sure the station name is spelled right, exactly like in main file under station_list
+		map = [
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			s, s, b, b, b, b, g, r,
+			s, s, b, b, b, b, g, r,
+		]
+		return map
+	elif current_station == "Prospect Park":	#Make sure the station name is spelled right, exactly like in main file under station_list
+		map = [
+			b, b, s, s, b, b, g, r,
+			b, b, s, s, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+		]
+		return map
+	elif current_station == "Winthrop St":	#Make sure the station name is spelled right, exactly like in main file under station_list
+		map = [
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, s, s,
+			b, b, o, y, b, b, s, s,
+			b, o, y, b, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+		]
+		return map
+	elif current_station == "Times Square - 42nd St":	#Make sure the station name is spelled right, exactly like in main file under station_list
+		map = [
+			b, b, b, r, y, b, b, b,
+			b, b, b, r, y, b, b, b,
+			b, b, b, r, y, b, b, b,
+			b, b, b, s, s, a, a, a,
+			v, v, v, s, s, v, v, v,
+			b, b, b, r, y, b, b, b,
+			b, b, b, r, b, y, b, b,
+			b, b, b, r, b, b, y, b,
+		]
+		return map
+	else:									#This is the backup map if no station map is found for current_station
+		map = [
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, b, o, y, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			b, o, y, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+			o, y, b, b, b, b, g, r,
+		]
+		return map
+
 def ones(T,B,digit):
 	if digit == "0":
 		led_display = [
