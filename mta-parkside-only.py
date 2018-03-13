7cd519ec79ac846dfc72ef132b3cc599e510d40b
Dan <dansushi@gmail.com>
2018-03-13 00:59:27 -0400

    Update mta-parkside-only.py

--- mta-parkside-only.py
+++ mta-parkside-only.py
@@ -71,6 +71,16 @@ def dec_to_bin(wt_dec):
 	wt_bin = bin(int(wt_dec))[2:] 
 	return wt_bin
 
+#flak
+def NoTrainInfo(): 
+	print("No train information available at the moment.")
+	sense.set_pixels(NoTrainWaitTimeInfo1())	# Sets screen with "waiting for train info" display
+	time.sleep(0.75)
+	sense.set_pixels(NoTrainWaitTimeInfo2())	# Sets screen with "waiting for train info" display
+	time.sleep(0.75)
+	sense.set_pixels(NoTrainWaitTimeInfo3())	# Sets screen with "waiting for train info" display
+	time.sleep(0.75)
+	
 # 20 pixels of black for separating elements on the SenseHat LED screen
 def black_pixels():
 	b = (0, 0, 0) # Black
@@ -90,7 +100,7 @@ def wt_bin_to_pixels(T,B,wt_bin):
 
 
 # Figures out the color of the number/train line (tr is train number)
-def determine_text_color(tr):
+def determine_text_color(tr,wts):
 	
 	#Colors     r    g    b
 	BLACK =  (  0,   0,   0)	# Background color (default BLACK)
@@ -120,9 +130,37 @@ def determine_text_color(tr):
 		return GREEN
 	elif wts[tr][0] == "J" or wts[tr][0] == "Z":
 		return BROWN
-	elif wts[tr][0] == "L" or wts[tr][0] == "S":
+	elif wts[tr][0] == "L" or wts[tr][0] == "S" or wts[tr][0] == "FS":
 		return GRAY
+
+# Helps the joystick left right function loop around to be able to select from any of the stations available
+def station_picker(right_or_left):
 	
+	global station_list
+	global current_station
+	global n
+
+	if right_or_left == "right":
+		if n < (len(station_list) - 1):				# If n is less than total number of stations minus one to account for index
+			n = n + 1
+			current_station = station_list[n]
+		else: 										# If n has reached the last station
+			n = 0									# Reset n to 0
+			current_station = station_list[n]
+			
+	elif right_or_left == "left":
+		if n > 0:									# If n is greater zero
+			n = n - 1
+			current_station = station_list[n]
+		else: 										# If n has reached zero
+			n = (len(station_list) - 1)				# Reset n to total number of stations minus one to account for index
+			current_station = station_list[n]		
+	else:
+		pass
+		
+	return current_station
+
+		
 '''----------------------------------------------------------------------------'''
 
 # JOYSTICK FUNCTIONS
@@ -133,25 +171,20 @@ def joystick_up(event):
 	global is_held
 	global which_direction
 
-	print("up event which_direction is" , which_direction)
 	if event.action == "pressed" and event.direction == "up":
-		print("Up")
 		if which_direction == "S": #If up_press on South, change to Both
-			which_direction = "N"														# <-- CHANGE BACK TO "B" when both mode is ready
-			print("S -> B")
+			which_direction = "B"
 			sense.set_pixels(N_B_S_display(which_direction))						# Displays up, both, or down mode
 		elif which_direction == "B":   #If up_press on Both, change to North
 			which_direction = "N"
-			print("B -> N")
 			sense.set_pixels(N_B_S_display(which_direction))						# Displays up, both, or down mode
 		else:                       #If up_press on North, keep at North
 			which_direction == "N"
-			print("N -> N")
 			sense.set_pixels(N_B_S_display(which_direction))						# Displays up, both, or down mode
 	if event.action == "held":
 		is_held = True
 	elif event.action == "released":
-		print("You released Up")
+		pass
 		
 def joystick_down(event):
 
@@ -159,25 +192,20 @@ def joystick_down(event):
 	global is_held
 	global which_direction
 	
-	print("down event which_direction is" , which_direction)
 	if event.action == "pressed" and event.direction == "down":
-		print("Down")
 		if which_direction == "N": #If down_press on North, change to Both
-			which_direction = "S"														# <-- CHANGE BACK TO "B" when both mode is ready
-			print("N -> B")
+			which_direction = "B"
 			sense.set_pixels(N_B_S_display(which_direction))						# Displays up, both, or down mode
 		elif which_direction == "B":   #If down_press on Both, change to South
 			which_direction = "S"
-			print("B -> S")
 			sense.set_pixels(N_B_S_display(which_direction))						# Displays up, both, or down mode
 		else:                       #If down_press on South, keep at South
 			which_direction == "S"
-			print("S -> S")
 			sense.set_pixels(N_B_S_display(which_direction))						# Displays up, both, or down mode
 	if event.action == "held":
 		is_held = True
 	elif event.action == "released":
-		print("You released Down")
+		pass
 
 def joystick_left(event):	# Cycles station to the previous one in the list
 
@@ -185,13 +213,12 @@ def joystick_left(event):	# Cycles station to the previous one in the list
 	global is_held
 	
 	if event.action == "pressed" and event.direction == "left":
-		print("Joystick left pressed")
 		current_station = station_picker("left")
 		sense.set_pixels(station_map(current_station[0]))						# Displays station map for selected station
 	if event.action == "held":
 		is_held = True
 	elif event.action == "released":
-		print("Joystick left released")
+		pass
 		
 	
 def joystick_right(event):	# Cycles station to the next one in the list
@@ -200,15 +227,12 @@ def joystick_right(event):	# Cycles station to the next one in the list
 	global is_held
 	
 	if event.action == "pressed" and event.direction == "right":
-		print("Joystick right pressed")
 		current_station = station_picker("right")
 		sense.set_pixels(station_map(current_station[0]))						# Displays station map for selected station
-	print("BLAH1")
 	if event.action == "held":
 		is_held = True
-		print("BLAH2")
 	elif event.action == "released":
-		print("Joystick right released")
+		pass
 		
 
 def joystick_middle(event):											#LATER, SET THIS TO EASY MODE STUFF
@@ -220,7 +244,7 @@ def joystick_middle(event):											#LATER, SET THIS TO EASY MODE STUFF
 '''----------------------------------------------------------------------------'''
 
 # LOGIC
-def run_logic_NS(current_station,which_direction):
+def run_logic_NorS(current_station,which_direction):
 
 	with urllib.request.urlopen("http://127.0.0.1:5000/by-id/" + current_station[1]) as url: #Pull info for chosen station
 		MTAPI_JSON = json.loads(url.read().decode()) # Loads MTAPI_JSON as a dictionary
@@ -248,16 +272,25 @@ def run_logic_NS(current_station,which_direction):
 			wts.append([route,wait_time])
 			#sense.show_message(wait_time)
 			print(str(route) + ': ' + str(wait_time) + 'm' + str(round((wait_time_mins - math.floor(wait_time_mins)) * 60)) + 's') # Prints train info line by line
+	
 
+def run_logic_Both(current_station,which_direction):
+	global wts
+	run_logic_NorS(current_station,"N")
+	N_wts = wts
+	run_logic_NorS(current_station,"S")
+	S_wts = wts
+	SenseHatDisplayBoth(N_wts,S_wts)
+	
 # DISPLAY
-def SenseHatDisplay():
+def SenseHatDisplayNorS():
 	
 	B = BLACK =  (  0,   0,   0)	# Background color (default BLACK)
 	T = WHITE =  (255, 255, 255)	# Text color (default WHITE)
 		
-	# Figures out the wait time for the first train (Digits at the top of the screen)
+	# Figures out the wait time for the 1st train (Digits at the top of the screen)
 	if len(wts) >= 1:	# Checks if there's a 1st train wait time available	
-		T = determine_text_color(0) # Figures out the color of the number/train line for first train
+		T = determine_text_color(0,wts) # Figures out the color of the number/train line for 1st train
 		first_wts = wts[0][1]		# Puts first train wait time into variable
 		#	first [] is item in list, i.e. next arriving train in order of arrival
 		#	second [] is route letter [0] / wait time [1]. LEAVE AT [1]
@@ -269,31 +302,23 @@ def SenseHatDisplay():
 	else: #If no train times available at all
 		ones_digit = black_pixels()[0:20] # Sets ones_digit pixels to black
 		tens_digit = black_pixels()[0:20] # Sets tens_digit pixels to black
-		print("No first train information")
-		sense.set_pixels(NoTrainWaitTimeInfo1())	# Sets screen with "waiting for train info" display
-		time.sleep(0.75)
-		sense.set_pixels(NoTrainWaitTimeInfo2())	# Sets screen with "waiting for train info" display
-		time.sleep(0.75)
-		sense.set_pixels(NoTrainWaitTimeInfo3())	# Sets screen with "waiting for train info" display
-		time.sleep(0.75)
+		NoTrainInfo()
 			
-	# Figures out the wait time for the second train (upper binary line)
+	# Figures out the wait time for the 2nd train (upper binary line)
 	if len(wts) >= 2:	# Checks if there's a 2nd train wait time available
-		T = determine_text_color(1) # Figures out the color of the number/train line for second train
+		T = determine_text_color(1,wts) # Figures out the color of the number/train line for 2nd train
 		wt_bin1 = dec_to_bin(wts[1][1])
 		wt_bin1_pixels = wt_bin_to_pixels(T,B,wt_bin1)
 	else:
 		wt_bin1_pixels = black_pixels() # Sets all pixels to black
-		print("No second train information")
 	
-	# Figures out the wait time for the third train (lower binary line)
+	# Figures out the wait time for the 3rd train (lower binary line)
 	if len(wts) >= 3:	# Checks if there's a 3rd train wait time available
-		T = determine_text_color(2) # Figures out the color of the number/train line for third train
+		T = determine_text_color(2,wts) # Figures out the color of the number/train line for 3rd train
 		wt_bin2 = dec_to_bin(wts[2][1])
 		wt_bin2_pixels = wt_bin_to_pixels(T,B,wt_bin2)
 	else:
 		wt_bin2_pixels = black_pixels() # Sets all pixels to black
-		print("No third train information")
 
 	#Sends the pixel information to the LED screen all together
 	sense.set_pixels(
@@ -307,33 +332,79 @@ def SenseHatDisplay():
 	wt_bin2_pixels[0:8]							# Pixel Line 8			Train 3 (Binary)
 	)
 
+def SenseHatDisplayBoth(N_wts,S_wts):
 	
-def station_picker(right_or_left):
+	B = BLACK =  (  0,   0,   0)	# Background color (default BLACK)
+	T = WHITE =  (255, 255, 255)	# Text color (default WHITE)
 	
-	global station_list
-	global current_station
-	global n
+	#NORTHBOUND TRAINS
+	
+	# Figures out the wait time for the 1st Northbound train
+	if len(N_wts) >= 1:	# Checks if there's a 1st train wait time available
+		T = determine_text_color(0,N_wts) # Figures out the color of the number/train line for 1st train
+		wts_n_bin1 = dec_to_bin(N_wts[0][1])
+		wts_n_bin1_pixels = wt_bin_to_pixels(T,B,wts_n_bin1)
+	else: #If no train times available at all
+		wts_n_bin1_pixels = black_pixels()[0:8]		# Sets first row to black
+		NoTrainInfo()
 
-	if right_or_left == "right":
-		if n < (len(station_list) - 1):				# If n is less than total number of stations (5) minus one to account for index
-			n = n + 1
-			current_station = station_list[n]
-		else: 										# If n has reached the last station (index = 4)
-			n = 0									# Reset n to 0
-			current_station = station_list[n]
-			
-	elif right_or_left == "left":
-		if n > 0:									# If n is greater zero
-			n = n - 1
-			current_station = station_list[n]
-		else: 										# If n has reached zero
-			n = (len(station_list) - 1)				# Reset n to total number of stations (5) minus one to account for index
-			current_station = station_list[n]		
+	# Figures out the wait time for the 2nd Northbound train
+	if len(N_wts) >= 2:	# Checks if there's a 2nd train wait time available
+		T = determine_text_color(1,N_wts) # Figures out the color of the number/train line for 2nd train
+		wts_n_bin2 = dec_to_bin(N_wts[1][1])
+		wts_n_bin2_pixels = wt_bin_to_pixels(T,B,wts_n_bin2)
 	else:
-		pass
-		
-	return current_station
+		wts_n_bin2_pixels = black_pixels() # Sets all pixels to black
+	
+	# Figures out the wait time for the 3rd Northbound train
+	if len(N_wts) >= 3:	# Checks if there's a 3rd train wait time available
+		T = determine_text_color(2,N_wts) # Figures out the color of the number/train line for 3rd train
+		wts_n_bin3 = dec_to_bin(N_wts[2][1])
+		wts_n_bin3_pixels = wt_bin_to_pixels(T,B,wts_n_bin3)
+	else:
+		wts_n_bin3_pixels = black_pixels() # Sets all pixels to black
 
+
+	#SOUTHBOUND TRAINS
+	
+	if len(S_wts) >= 1:	# Checks if there's a 1st train wait time available
+		T = determine_text_color(0,S_wts) # Figures out the color of the number/train line for 1st train
+		wts_s_bin1 = dec_to_bin(S_wts[0][1])
+		wts_s_bin1_pixels = wt_bin_to_pixels(T,B,wts_s_bin1)
+	else: #If no train times available at all
+		wts_s_bin1_pixels = black_pixels()[0:8]		# Sets first row to black
+		NoTrainInfo()
+
+	# Figures out the wait time for the 2nd Northbound train
+	if len(S_wts) >= 2:	# Checks if there's a 2nd train wait time available
+		T = determine_text_color(1,S_wts) # Figures out the color of the number/train line for 2nd train
+		wts_s_bin2 = dec_to_bin(S_wts[1][1])
+		wts_s_bin2_pixels = wt_bin_to_pixels(T,B,wts_s_bin2)
+	else:
+		wts_s_bin2_pixels = black_pixels() # Sets all pixels to black
+	
+	# Figures out the wait time for the 3rd Northbound train
+	if len(S_wts) >= 3:	# Checks if there's a 3rd train wait time available
+		T = determine_text_color(2,S_wts) # Figures out the color of the number/train line for 3rd train
+		wts_s_bin3 = dec_to_bin(S_wts[2][1])
+		wts_s_bin3_pixels = wt_bin_to_pixels(T,B,wts_s_bin3)
+	else:
+		wts_s_bin3_pixels = black_pixels() # Sets all pixels to black
+		
+		
+	#Sends the pixel information to the LED screen all together
+	sense.set_pixels(
+	wts_n_bin1_pixels[0:8] +						# Pixel Line 1		Northbound Train 1 (Binary)
+	wts_n_bin2_pixels[0:8] +						# Pixel Line 2		Northbound Train 2 (Binary)
+	wts_n_bin3_pixels[0:8] +						# Pixel Line 3		Northbound Train 3 (Binary)
+	black_pixels()[0:8] +						# Pixel Line 4
+	black_pixels()[0:8] +						# Pixel Line 5
+	wts_s_bin1_pixels[0:8] + 						# Pixel Line 6		Southbound Train 1 (Binary)
+	wts_s_bin2_pixels[0:8] +				 		# Pixel Line 7		Southbound Train 2 (Binary)
+	wts_s_bin3_pixels[0:8]							# Pixel Line 8		Southbound Train 3 (Binary)
+	)
+
+	
 #MAIN
 def main():
 	global is_held
@@ -353,8 +424,11 @@ def main():
 	while True:
 		try:
 			if subprocess.call(['curl', '-s', 'http://127.0.0.1:5000', '-o' , '/dev/null']) == 0 and is_held == False:	#If connection is okay (code 0) and joystick isn't being held
-				run_logic_NS(current_station,which_direction)						#Then run logic
-				SenseHatDisplay()
+				if which_direction == "N" or which_direction == "S":
+					run_logic_NorS(current_station,which_direction)						#Then run logic North or South
+					SenseHatDisplayNorS()
+				else:																#Else both
+					run_logic_Both(current_station,which_direction)						#Then run logic Both
 				time.sleep(1.5)
 			elif is_held == True:
 				is_held = False
